a9c1b955879d1786755e68aefe25f768556ed8ef
SuperKay <adegbesanoluwakayode52@gmail.com>
2026-05-18 10:48:33 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState, useRef, useEffect } from 'react'
-import { FaArrowDown, FaChevronDown, FaRegistered, FaSignOutAlt } from 'react-icons/fa'
+import { FaArrowDown, FaBars, FaChevronDown, FaRegistered, FaSignOutAlt, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
     const [openDropdown, setOpenDropdown] = useState<string | null>(null)
     const timeoutRef = useRef<NodeJS.Timeout | null>(null)
 
@@ -38,7 +39,8 @@ const Navbar = () => {
     }, [])
 
     return (
-        <div className='absolute top-25 flex justify-between items-center z-[60] w-full px-8'>
+        <div className="">
+            <div className=' hidden lg:flex absolute top-25 flex justify-between items-center z-[60] w-full px-8'>
             {/* Logo */}
             <div>
                 <Image
@@ -133,10 +135,120 @@ const Navbar = () => {
             </div>
             <div className="">
                 <Link href="/" className='flex gap-2 bg-[var(--orange)] px-2 py-1 rounded-sm items-center text-[var(--white)] text-sm transition-colors'>
-                            <FaSignOutAlt className='text-[var(--white)]'/>
-                            Register Now
-                        </Link>
+                 <FaSignOutAlt className='text-[var(--white)]'/>
+                Register Now
+                </Link>
+            </div>   
+        </div>
+        {/* Mobile */}
+        <div className="lg:hidden">
+            <div className='absolute top-25 flex justify-between items-center z-[60] w-full px-8'>
+            {/* Logo */}
+            <div>
+                <Image
+                    src='/images/logo2.png'
+                    alt='Navbar logo'
+                    width={180}
+                    height={60}
+                    priority
+                />
             </div>
+            <button type='button'
+            onClick={()=> setIsOpen(!isOpen)}>
+                {isOpen? <FaTimes size={25} className="text-[var(--blue)] hover:text-[var(--white)] hover:bg-[var(--blue)] p-1"/>
+                 : 
+                 <FaBars size={25} className="text-[var(--blue)] hover:text-[var(--white)] hover:bg-[var(--blue)] p-1" />}
+            </button>
+
+            {isOpen && (
+                <div className="flex flex-col gap-6 items-left text-black w-full  bg-white py-8 rounded-lg shadow-lg absolute top-full left-0 w-full mt-2 z-50">
+                    <ul>
+                        <li>
+                        <Link href="/" className='hover:text-[var(--orange)] hover:bg-[var(--orange)]/20 w-full transition-colors '>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className='hover:text-[var(--orange)] transition-colors'>
+                            About
+                        </Link>
+                    </li>
+                    {/* ==================== COURSES DROPDOWN ==================== */}
+                    <li 
+                        className="relative"
+                        onMouseEnter={() => handleMouseEnter('courses')}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <Link 
+                            href="/" 
+                            className='flex items-center text-left gap-1 hover:text-[var(--orange)] transition-colors  py-2 rounded-lg'
+                        >
+                            Courses
+                            <FaChevronDown size={14} />
+                        </Link>
+
+                        {openDropdown === 'courses' && (
+                            <ul className='absolute left-0 top-full mt-2 bg-[var(--orange)] text-white shadow-lg rounded-md py-3 w-48 z-50'>
+                                <li>
+                                    <Link href="/" className='block px-5 py-2 hover:bg-white/20'>All Courses</Link>
+                                </li>
+                                <li>
+                                    <Link href="/" className='block px-5 py-2 hover:bg-white/20'>Milestone</Link>
+                                </li>
+                            </ul>
+                        )}
+                    </li>
+                    
+                    <li>
+                        <Link href="/" className='hover:text-[var(--orange)] transition-colors'>
+                            Publications
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/" className='hover:text-[var(--orange)] transition-colors'>
+                            Contact
+                        </Link>
+                    </li>
+                    {/* ==================== FEATURES DROPDOWN ==================== */}
+                    <li 
+                        className="relative"
+                        onMouseEnter={() => handleMouseEnter('features')}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <Link 
+                            href="/" 
+                            className='flex items-center gap-1 hover:text-[var(--orange)] transition-colors  py-2 rounded-lg'
+                        >
+                            Features
+                            <FaChevronDown size={14} />
+                        </Link>
+
+                        {openDropdown === 'features' && (
+                            <ul className='absolute left-0 top-full mt-2 bg-[var(--orange)] text-white shadow-lg rounded-md py-3 w-52 z-50'>
+                                <li>
+                                    <Link href="/" className='block px-5 py-2 hover:bg-white/20'>Our Service</Link>
+                                </li>
+                                <li>
+                                    <Link href="/" className='block px-5 py-2 hover:bg-white/20'>Data Bank</Link>
+                                </li>
+                                <li>
+                                    <Link href="/" className='block px-5 py-2 hover:bg-white/20'>Publication</Link>
+                                </li>
+                            </ul>         
+                        )}
+                        <div className="">
+                <Link href="/" className='flex gap-2 bg-[var(--orange)] px-2 py-1 rounded-sm items-center text-[var(--white)] text-sm transition-colors'>
+                 <FaSignOutAlt className='text-[var(--white)]'/>
+                Register Now
+                </Link>
+            </div>   
+                    </li>
+                    </ul>
+                </div>
+                
+            )}
+            </div>
+        </div>
         </div>
     )
 }
